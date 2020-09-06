35fe5ba6fd49ecacb1acd6ea19854bc65e48b9fc
ISHAN SINGHAL <ishan-cs719@dayanandasagar.edu>
2020-09-06 11:51:16 +0530

    Add files via upload

--- all.dart
+++ all.dart
@@ -23,6 +23,7 @@ void main() {
   );
 }
 
+
 #home.dart
 import 'package:flutter/material.dart';
 
@@ -79,6 +80,7 @@ class _MyHomeState extends State<MyHome> {
     );
   }
 }
+
 
 #reg.dart
 import 'package:firebase_auth/firebase_auth.dart';
@@ -172,6 +174,96 @@ class _MyRegState extends State<MyReg> {
     );
   }
 }
+
+
+#login.dart
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatefulWidget {
+  @override
+  _MyLoginState createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  var authc = FirebaseAuth.instance;
+
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Material(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(10),
+                elevation: 10,
+                child: MaterialButton(
+                  minWidth: 200,
+                  height: 40,
+                  child: Text('Login'),
+                  onPressed: () async {
+                    try {
+                      var userSignin = await authc.signInWithEmailAndPassword(
+                          email: email, password: password);
+                      print(userSignin);
+
+                      if (userSignin != null) {
+                        Navigator.pushNamed(context, "chat");
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 #chat.dart
 import 'package:firebase_auth/firebase_auth.dart';
